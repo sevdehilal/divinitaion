3425e36d9b02292bbbaf9ed225795a932b389cdb
ferenbulbul61 <ferenbulbul@gmail.com>
2024-11-25 17:15:48 +0300

    tiwtter added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
+import 'package:divinitaion/Services/twitter.dart';
 import 'package:divinitaion/Widgets/ClientWidgets/client_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   HttpOverrides.global = MyHttpOverrides();
   runApp(MaterialApp(
-    home: CustomBottomNavigation(),
+    home: MyApp(),
     debugShowCheckedModeBanner: false,
   ));
 }
